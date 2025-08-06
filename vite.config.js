8aeb6f9ca5547fe2060181b218c3309defaee164
GWSpakman <gwspakman@outlook.com>
2025-08-06 23:23:21 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: https://gwspakman.github.io/09_vite,
+  base: '/09_vite/',
 })
